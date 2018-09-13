249cffe927867dbcce926b66d09ddf205cec2043
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-09-13 19:24:11 +0530

    Update variables.tf

--- modules/EC2/variables.tf
+++ modules/EC2/variables.tf
@@ -1,33 +1,13 @@
-variable "ami" {
-  description = "ID of AMI to use for the instance"
-  default = "ami-efa5fe80"
-}
-variable "instance_type" {
-  description = "The type of instance to start"
-  default = "t2.micro"
-}
-variable "key_name" {
-  description = "The key name to use for the instance"
-  default     = "jai_terraform"
-}
-variable "subnet_id" {
-  description = "The VPC Subnet ID to launch in"
-  default    = "subnet-18008455"
-}
-variable "security_groups" {
-  description = "A list of security group IDs to assign to the ELB"
-  type        = "list"
-  default = ["sg-74f26f1f"]
-}
-variable "associate_public_ip_address" {
-  description = "Associate a public ip address with an instance in a VPC"
-  default     = true
-}
-variable "tags" {
-  description = "A list of tag blocks. Each element should have keys named key, value, and propagate_at_launch."
-  default     = "EC2-wordpress"
-}
-variable "AWS_REGION" {
-  default = "ap-south-1"
-}
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
 
+
+variable "ami" {}
+variable "instance_type" {}
+variable  "key_name" {} 
+variable "ec2_subnet" {}
+variable "security_group" {}
+variable "vpc_id" {
+  default = "vpc-0ef67e6b"
+}
