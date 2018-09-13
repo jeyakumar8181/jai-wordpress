1a3d5eb22dc2eb4c1d0af9171dec3963cfa85d9b
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-09-13 19:41:18 +0530

    Update variable.tf

--- modules/ELB/variable.tf
+++ modules/ELB/variable.tf
@@ -7,5 +7,28 @@ variable "vpc_id" {
 variable "server_id" {}
 variable "elb_subnet" {}
 variable "instance_id" {}
+variable "elb_subnet" {}
+variable "instance_id" {}
+variable "cross_zone_load_balancing" {
+  description = "Enable cross-zone load balancing"
+  default     = true
+}
+
+variable "idle_timeout" {
+  description = "The time in seconds that the connection is allowed to be idle"
+  default     = 400
+}
+
+variable "connection_draining" {
+  description = "Boolean to enable connection draining"
+  default     = true
+}
+
+variable "connection_draining_timeout" {
+  description = "The time in seconds to allow for connections to drain"
+  default     = 300
+}
+variable "instance_id" {}
+
 
 
