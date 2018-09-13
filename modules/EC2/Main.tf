6b6b4ce8c907a0c51b8b6a16a26ac696b70aabd3
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-09-13 19:22:45 +0530

    Update Main.tf

--- modules/EC2/Main.tf
+++ modules/EC2/Main.tf
@@ -1,12 +1,14 @@
 provider "aws" {
-        region = "${var.AWS_REGION}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
-resource "aws_instance" "wordpress-jai" {
+resource "aws_instance" "ebizon" {
         ami = "${var.ami}"
         instance_type = "${var.instance_type}"
         key_name = "${var.key_name}"
-        subnet_id = "${var.subnet_id}"
+        subnet_id = ["${var.ec2_subnet}"]
         security_groups = ["${var.security_groups}"]
         associate_public_ip_address = "${var.associate_public_ip_address}"
         user_data = "${file("./modules/EC2/wordpress.sh")}"
