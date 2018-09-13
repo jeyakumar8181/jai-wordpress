25ff038846e28257582ba59e956cf1f7f12c0b87
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-09-13 19:27:39 +0530

    Update variable.tf

--- modules/ELB/variable.tf
+++ modules/ELB/variable.tf
@@ -1,38 +1,11 @@
 variable "name"{
   default = "wordpress-ELB"
 }
-variable "security_groups" {
-  description = "A list of security group IDs to assign to the ELB"
-  type        = "list"
-  default = ["sg-74f26f1f"]
+variable "vpc_id" {
+  default = "vpc-0ef67e6b"
 }
-
-variable "subnets" {
-  description = "A list of subnet IDs to attach to the ELB"
-  type = "list"
-  default     = ["subnet-18008455"]
-}
-
-
-variable "cross_zone_load_balancing" {
-  description = "Enable cross-zone load balancing"
-  default     = true
-}
-
-variable "idle_timeout" {
-  description = "The time in seconds that the connection is allowed to be idle"
-  default     = 400
-}
-
-variable "connection_draining" {
-  description = "Boolean to enable connection draining"
-  default     = true
-}
-
-variable "connection_draining_timeout" {
-  description = "The time in seconds to allow for connections to drain"
-  default     = 300
-}
+variable "server_id" {}
+variable "elb_subnet" {}
 variable "instance_id" {}
 
 
